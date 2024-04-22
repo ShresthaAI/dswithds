28032d154c3255a683fb3a12c90ea65b3515e708
dexhrestha <dipesh.shrestha009@gmail.com>
2024-04-22 13:35:44 +0100

    Interactive Streamlit App

---
+++ streamlit/pages/1_LinearAlgebra.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Linear Algebra", page_icon="")
+
+
+st.markdown("# Linear Algebra")
+
+st.sidebar.header("LinearAlgebra")
+
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
