43797e41d472bc525921724da983038f8b3636b4
dexhrestha <dipesh.shrestha009@gmail.com>
2024-04-23 11:07:17 +0100

    Added vector operation for lin alg

--- app/pages/1_Linear Algebra.py
+++ app/pages/1_Linear Algebra.py
@@ -71,7 +71,7 @@ st.code(
 
 st.markdown('''### An arrow''')
 
-vector = [st.number_input("X1",value=1.0,min_value=0.0,max_value=5.0,step=0.5,key='x1'),st.number_input("X1",value=1.0,min_value=0.0,max_value=5.0,step=0.5,key='y1')]
+vector = [st.number_input("X1",value=1.0,min_value=-5.0,max_value=5.0,step=0.5,key='x1'),st.number_input("Y1",value=1.0,min_value=-5.0,max_value=5.0,step=0.5,key='y1')]
 st.write(f"[{vector[0]} {vector[1]}]")
 # Example usage
 fig = utils.visualize_vector(title="Vector Visualization", show=False,
@@ -99,9 +99,30 @@ where $||v||$ is the magnitude of the vector. Normalization is often used in var
 """
 )
 
-vector = np.array([st.number_input("X1",value=1.0,min_value=0.0,max_value=5.0,step=0.5,key='norm_x1'),st.number_input("X1",value=1.0,min_value=0.0,max_value=5.0,step=0.5,key='norm_y1')])
-st.write(f"[{vector[0]} {vector[1]}]")
+vector = np.array([st.number_input("X1",value=1.0,min_value=-5.0,max_value=5.0,step=0.5,key='norm_x1'),st.number_input("y1",value=1.0,min_value=-5.0,max_value=5.0,step=0.5,key='norm_y1')])
+st.write(f"**Normalized vector :**[{2*vector[0]} {2*vector[1]}]")
 norm = utils.normalize_vector(vector)
 fig = utils.visualize_vector(v=vector,norm=norm,title="Arrow representation of vector and normalized form ")
+st.pyplot(fig=fig)
+
+st.markdown("## Vector Operations")
+st.markdown("### Scalar multiplication")
+
+vector = np.array([st.number_input("X1",value=1.0,min_value=-5.0,max_value=5.0,step=0.5,key='scaled_X1'),st.number_input("Y1",value=2.0,min_value=-5.0,max_value=5.0,step=0.5,key='scaled_y1')])
+st.write(f"**Scaled vector :**[{2*vector[0]} {2*vector[1]}]")
+
+fig = utils.visualize_vector(v2=2*vector,v=vector,title="Scalar multiplication",grid_length=10)
+st.pyplot(fig=fig)
+
+st.markdown("### Vector Addition")
+
+
+
+v1 = np.array([st.number_input("X1",value=1.0,min_value=-5.0,max_value=5.0,step=0.5,key='add_x1'),st.number_input("Y1",value=2.0,min_value=-5.0,max_value=5.0,step=0.5,key='add_y1')])
+v2 = np.array([st.number_input("X2",value=-2.0,min_value=-5.0,max_value=5.0,step=0.5,key='add_x2'),st.number_input("Y2",value=3.0,min_value=-5.0,max_value=5.0,step=0.5,key='add_y2')])
+v3 = v1 + v2
+st.write(f"**V3 :**[{v3[0]} {v2[1]}]")
+
+fig = utils.visualize_vector(v1=v1,v2=v2,v3=v3,title="Vector Addition",grid_length=10)
 st.pyplot(fig=fig)
 
